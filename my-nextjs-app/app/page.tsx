15587fa5fe9cc585915efd1a06860cbd71ba89ca
Fillip Novak Grunnsund <sheik1117@gmail.com>
2023-10-31 19:42:04 +0100

    fix index page

--- my-nextjs-app/app/page.tsx
+++ my-nextjs-app/app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a href="http://localhost:3000/api/getlist">Go to list</a>
+      <a href="http://localhost:3000/api/getlist">Get collection</a>
+      <a href="http://localhost:3000/api/reguser">Post "data"</a>
     </main>
   )
 }
